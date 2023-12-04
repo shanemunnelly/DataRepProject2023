d0f22c2c68c4c9491b3c943889f39071618d50a9
unknown <g00381223@atu.ie>
2023-12-04 14:47:39 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 4000
 const cors = require('cors');
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
+
 
 
 app.use(cors());
@@ -84,6 +88,11 @@ app.put('/api/book/:identifier', async(req, res) => {
   let book = await bookModel.findByIdAndUpdate(req.params.identifier, req.body, {new:true} );
   res.send(book);
 })
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../build/index.html'));
+  });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
